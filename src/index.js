50401e784ce00c799a4514dfe1778be079e3535d
CsabaK <898182+iexa@users.noreply.github.com>
2024-05-03 18:14:06 +0200

    query strings + cookies

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
 import express from "express";
 import helmet from "helmet";
-import { getAbsolutePath } from "./utils/esm-path.js";
+import cookieParser from "cookie-parser";
+import { getAbsolutePath } from "./utils/esm-path.js"; // from node21 not needed
 
 const app = express();
-app.use(helmet()); // security
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        "script-src": ["'self'", "example.com"],
+      },
+    },
+  })
+); // security
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(express.static(getAbsolutePath(import.meta.url, "../pub")));
+app.set("views", getAbsolutePath(import.meta.url, "views"));
+app.set("view engine", "ejs");
+// https://stackoverflow.com/questions/39781767/using-nunjucks-in-an-express-app
 
-app.post("/ajax", async (req, res) => {
-  console.log("ajax post");
-  res.json({ data: "hello!" });
-});
+app
+  .get("/", async (req, res) => {
+    let username = req.cookies.username;
+    let logged_in = username ? true : false;
+    let login_failed = req.query.loginfailed;
+    console.log(req.query);
+    res.render("index", { logged_in, login_failed, username });
+  })
+  .post("/login", async (req, res) => {
+    const { password, username } = req.body;
+    if (password === "letmein" && username.trim() !== "") {
+      res.cookie("username", username, {
+        maxAge: 3600 * 1000,
+        httpOnly: true,
+      });
+      res.redirect("/");
+    } else {
+      res.redirect("/?loginfailed=1");
+    }
+    // console.log("pass should be letmein...");
+  })
+  .get("/logout", async (req, res) => {
+    res.clearCookie("username");
+    res.redirect("/");
+  });
 
 app.listen(process.env.PORT || 3000, () =>
   console.log(`listening at ::1:${process.env.PORT || 3000}`)
